a0cb191b160c5b745943b0f326988b69636c7180
jmbothe <jeffrymbothe@gmail.com>
2018-02-09 19:25:17 -0500

    add bubbleSort(), just because

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 module.exports = Object.assign(
   {},
-  require('./lib/algorithms/find-missing-number')
+  require('./lib/algorithms/find-missing-number'),
+  require('./lib/algorithms/merge-sort'),
+  require('./lib/algorithms/quick-sort'),
+  require('./lib/algorithms/bubble-sort'),
 );
