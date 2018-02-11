383d37b715eadf78cdcc1f61d0c7cd645415a177
jmbothe <jeffrymbothe@gmail.com>
2018-02-11 11:20:51 -0500

    experimenting with doc tools

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 module.exports = Object.assign(
   {},
-  require('./lib/algorithms/find-missing-number'),
-  require('./lib/algorithms/merge-sort'),
-  require('./lib/algorithms/quick-sort'),
-  require('./lib/algorithms/bubble-sort'),
+  require('./lib'),
 );
