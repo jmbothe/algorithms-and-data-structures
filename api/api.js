88ea84634dfc03c5185aeb89b8187d3d76c85869
jmbothe <jeffrymbothe@gmail.com>
2018-02-12 13:50:23 -0500

    edit api script

--- api/api.js
+++ api/api.js
@@ -4,19 +4,10 @@ so we need this script just to add the title line to the API documentation ¯\_(
 */
 const fs = require('fs');
 
-const temp = fs.createReadStream('api/temp.md');
-const final = fs.createWriteStream('api/API.md');
-/*
-node.js doesnt have a prependFile method on fs,
-so we need to create a new file with just the title line
-and then append the rest of the API document to the new file ¯\_(ツ)_/¯wtf¯\_(ツ)_/¯
-*/
-final.write('# API for Alogrithms and Data Structures\n\n', () => {
-  temp.on('data', (chunk) => {
-    fs.appendFileSync('api/API.md', chunk);
+const apiFile = fs.createWriteStream('api/API.md');
+
+apiFile.write('# API for Alogrithms and Data Structures\n\n', () => {
+  process.stdin.on('data', (chunk) => {
+    apiFile.write(chunk);
   });
-});
-// delete temp file :D
-temp.on('end', () => {
-  fs.unlinkSync('api/temp.md');
 });
